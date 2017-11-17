dc58771d8bc2cb9d91731c3688f2115bed78a71a
qianhaoq <362609254@qq.com>
2017-11-17 21:58:43 +0800

    apache配置成功

--- build.sh
+++ build.sh
@@ -3,7 +3,9 @@ set -x
 
 rm -rf /root/backup/*
 
-cp -r /var/www/html/* /root/backup/
+# cp -r /var/www/html/* /root/backup/
+cp -r /root/webroot/* /root/backup/
 
-cp -r /root/git/raichu.cn/* /var/www/html/
+# cp -r /root/git/raichu.cn/* /var/www/html/
+cp -r /root/git/raichu.cn/* /root/webroot/
 
